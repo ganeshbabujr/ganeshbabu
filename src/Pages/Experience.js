0a6eba75d37ab7345a15aa2da316ca79b0987285
Ganesh <ganeshbabujaisankar5@gmail.com>
2025-05-02 23:06:39 +0530

    Update Experience.js

--- src/Pages/Experience.js
+++ src/Pages/Experience.js
@@ -65,7 +65,7 @@ function Experience() {
             <ProgressBar percent="80%" title='MS SQL' /> */}
 
             <BodyCard header={'Languages'} skills={['SQL', 'PL/SQL', 'Unix Shell Scripts', 'Python','HTML5/CSS']}/>
-            <BodyCard header={'Tool'} skills={['SQL Developer', 'Toad','SQL Plus', 'SVN', 'Git', 'Jira']}/>
+            <BodyCard header={'Tool'} skills={['SQL Developer', 'Toad','SQL Plus', 'SVN', 'Jira']}/>
             <BodyCard header={'Database'} skills={['Oracle 11g','Oracle 12c']}/>
             <BodyCard header={'Job Scheduling'} skills={['Autosys,', 'Control-M']}/>
             
