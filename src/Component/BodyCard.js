c4b3b9d51f682f6ba9878c830eb00eff67e52f1d
Ganesh <ganeshbabujaisankar5@gmail.com>
2025-05-03 15:01:15 +0530

    Update BodyCard.js

--- src/Component/BodyCard.js
+++ src/Component/BodyCard.js
@@ -1,21 +1,35 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 function BodyCard({ header, skills, className }) {
     return (
-        <div className={`frame ${className}`} style={{ width: '30%', height: 'unset', flex:'1 1 200px' }}>
-            <div className='bodycard-content'>
-                <div className=' f2 bodycard-header'>
+        <div
+            className={`frame ${className}`}
+            style={{ width: '30%', height: 'unset', flex: '1 1 200px' }}
+        >
+            <div className="bodycard-content">
+                <div className="f2 bodycard-header">
                     {header.toUpperCase()}
                 </div>
                 <ul>
-                    {skills?.map((sk, index) => (
-                        <li key={index}>{sk}</li>
+                    {skills.map((sk, index) => (
+                        <li key={index}>• {sk}</li>
                     ))}
                 </ul>
             </div>
-
         </div>
-    )
+    );
 }
 
-export default BodyCard
+BodyCard.propTypes = {
+    header: PropTypes.string.isRequired,
+    skills: PropTypes.arrayOf(PropTypes.string),
+    className: PropTypes.string
+};
+
+BodyCard.defaultProps = {
+    skills: [],
+    className: ''
+};
+
+export default BodyCard;
