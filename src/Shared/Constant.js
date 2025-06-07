99346899e4462cc3faadc435de60d75b662562ca
Ganesh <ganeshbabujaisankar5@gmail.com>
2025-06-07 13:21:29 +0530

    Update Constant.js

--- src/Shared/Constant.js
+++ src/Shared/Constant.js
@@ -1,6 +1,6 @@
 export const currentCompanyName = 'Swatantra Systems Pvt Ltd';
 export const currentRole = 'PL/SQL Developer';
-export const currentExperience = '4.6 Years';
+export const currentExperience = '4.8 Years';
 export const currentMobileNo = '+918667799658';
 export const currentEmail = 'ganeshbabujaisankar5@gmail.com';
 export const currentLinkedinUrl = 'https://www.linkedin.com/in/ganeshbabujr/';
@@ -11,7 +11,7 @@ export const oldCompanyHistory = [
     {
         name: 'N2S Technologies Pvt Ltd',
         designation: 'WordPress Developer',
-        startDate: 'July 2019',
+        startDate: 'December 2018',
         endDate: 'September 2020',
         location: 'Chennai',
         isCurrentCompany : false,
