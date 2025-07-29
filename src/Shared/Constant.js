8c15494ea959c92370d020b20b58043b76026cc5
Ganesh <ganeshbabujaisankar5@gmail.com>
2025-07-29 16:51:37 +0530

    Update Constant.js

--- src/Shared/Constant.js
+++ src/Shared/Constant.js
@@ -2,7 +2,7 @@ export const currentCompanyName = 'Swatantra Systems Pvt Ltd';
 export const currentRole = 'PL/SQL Developer';
 export const currentExperience = '4.8 Years';
 export const currentMobileNo = '+918667799658';
-export const currentEmail = 'jrganeshbabu5@gmail.com';
+export const currentEmail = 'jr.ganesh6@gmail.com';
 export const currentLinkedinUrl = 'https://www.linkedin.com/in/ganeshbabujr/';
 export const currentGithubUrl = 'https://github.com/ganeshbabujr';
 export const currentzcompanyStartDate = 'October 2020';
@@ -11,7 +11,7 @@ export const oldCompanyHistory = [
     {
         name: 'N2S Technologies Pvt Ltd',
         designation: 'WordPress Developer',
-        startDate: 'December 2018',
+        startDate: 'July 2019',
         endDate: 'September 2020',
         location: 'Chennai',
         isCurrentCompany : false,
@@ -22,8 +22,16 @@ export const oldCompanyHistory = [
         designation: 'PL/SQL Developer',
         startDate: 'October 2020',
         location: 'Hyderabad',
-        isCurrentCompany : true,
+        isCurrentCompany : false,
         id : 2
+    },
+        {
+        name: 'Oracle Financial Services Software',
+        designation: 'Senior Consultant',
+        startDate: 'August 2025',
+        location: 'Chennai',
+        isCurrentCompany : true,
+        id : 3
     }
 ];
 
